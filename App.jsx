4bbfdba93c4870858f5843153685e4caf18e9665
rffzahra <86545058+rffzahra@users.noreply.github.com>
2021-06-27 10:51:29 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha.We converted
+          everyting from Scratch including Javascript React.js,HTML
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha.We converted
+          everyting from Scratch including Javascript React.js,HTML
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha.We converted
+          everyting from Scratch including Javascript React.js,HTML
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha.We converted
+          everyting from Scratch including Javascript React.js,HTML
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha.We converted
+          everyting from Scratch including Javascript React.js,HTML
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha.We converted
+          everyting from Scratch including Javascript React.js,HTML
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha.We converted
+          everyting from Scratch including Javascript React.js,HTML
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Sinha.We converted
+          everyting from Scratch including Javascript React.js,HTML
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
